6f2960ba93e4d50c48a525be29c42a33fbed27a5
jayantSparmar <99529680+jayantSparmar@users.noreply.github.com>
2022-03-04 12:00:44 +0530

    Add files via upload

---
+++ src/com/Emp_wage_uc1.java
@@ -0,0 +1,21 @@
+package com.bridgelaqbz;
+
+public class Emp_wage_uc1 {
+
+    public static void main(String[] args) {
+        // practise uc1
+        
+	int is_full_time = 1;
+    double empCheck = Math.floor(Math.random()*10)%2;
+
+    if
+    (empCheck == is_full_time)
+
+        System.out.println("if employee is present ");
+    else
+        System.out.println("if employee is Absent");
+
+
+
+    }
+}
